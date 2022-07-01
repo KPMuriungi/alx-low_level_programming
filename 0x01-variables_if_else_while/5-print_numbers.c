efc15edc288bc34f1c1eb3996ef1de19ce4c4e48
KPMuriungi <kpmuriungi@gmail.com>
2022-07-01 21:47:48 +0000

    0x01

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function tells whether number is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
