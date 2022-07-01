47df04823e148ee9c1b906d12c476cd33df69890
KPMuriungi <kpmuriungi@gmail.com>
2022-07-01 22:54:15 +0000

    0x01

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function tells whether number is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
