55e656599cf1cddefe7f2ef8a29bd6af5d7cf6c0
KPMuriungi <kpmuriungi@gmail.com>
2022-07-06 09:38:11 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase characters
+ *
+ * @c: ASCII Character
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	char c;
+
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
