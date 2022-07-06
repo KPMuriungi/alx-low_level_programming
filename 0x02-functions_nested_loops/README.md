f61e50efc40aab415105464eabe9d7737cde567b
KPMuriungi <kpmuriungi@gmail.com>
2022-07-05 17:05:14 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This folder contains C variables, if, else amd while tasks.
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function tells whether number is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
