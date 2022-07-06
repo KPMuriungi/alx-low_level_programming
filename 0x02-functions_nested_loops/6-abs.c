0d9f7cd4790efa3600670be252839eac89498161
KPMuriungi <kpmuriungi@gmail.com>
2022-07-06 10:31:39 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * @i: Parameter
+ *
+ * Return: 0
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return ((-1) * i);
+	}
+}
