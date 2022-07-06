dea7dadd5096b01c22f91940884634341baa774a
KPMuriungi <kpmuriungi@gmail.com>
2022-07-06 22:32:07 +0000

    Nested Loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds two parameters
+ *
+ * @i: Parameter
+ *
+ * @j: Parameter
+ *
+ * Return: sum
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	return (c);
+}
