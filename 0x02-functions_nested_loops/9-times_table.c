0868dedc4a1b6d301ac98227340fef3f75098095
KPMuriungi <kpmuriungi@gmail.com>
2022-07-06 21:39:48 +0000

    Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,17 @@ void times_table(void)
 			z = x * y;
 			if (z == 0)
 			{
-				_putchar(z + '0');
+				if (y < 9)
+				{
+					_putchar(z + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(z + '0');
+				}
 			}
 			else if (z < 10 && z != 0)
 			{
