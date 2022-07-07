8b8a08ea99d73d3df3804d3d8a1b4730cb979aa6
KPMuriungi <kpmuriungi@gmail.com>
2022-07-07 21:01:16 +0000

    More funtions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (c = 0 ; c < 15 ; c++)
+		for (c = 0; c < 15; c++)
 		{
 			if (c > 9)
-			{
+			
 				_putchar((c / 10) + '0')
-			}
-		_putchar((j % 10) + '0')
+				_putchar((j % 10) + '0')
 		}
 		_putchar('\n');
 	}
