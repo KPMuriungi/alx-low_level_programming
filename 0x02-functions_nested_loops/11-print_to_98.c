3a1b046d9b8668fc44d8b10f548d9074577b9c0c
KPMuriungi <kpmuriungi@gmail.com>
2022-07-07 00:42:23 +0000

    Nested Loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,16 @@
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
 		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
+		}
+		while (n == 98)
+		{
+			printf("%d", n);
 		}
 	}
 	else if (n < 98)
