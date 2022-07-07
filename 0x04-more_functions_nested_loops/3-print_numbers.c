f4808eeb1f906735618e617ebc98246471b9c4f1
KPMuriungi <kpmuriungi@gmail.com>
2022-07-07 20:13:53 +0000

    More funtions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
