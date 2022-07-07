5c7f07af120d81fb71f182790b06922d8cb2c22d
KPMuriungi <kpmuriungi@gmail.com>
2022-07-07 02:37:48 -0700

    Debugging

--- 0x03-debugging/2-test.c
+++ 0x03-debugging/2-test.c
@@ -13,19 +13,27 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	else if
+	else if (a > c && c > b)
+	{
+		largest = a;
+	}
+	else if (b > c && c > a)
+	{
+		largest = b;
+	}
+	else if (c > b && b > a)
+	{
+		largest = c;
+	}
+	else if (c >a && a > b)
 	{
 		largest = c;
 	}
