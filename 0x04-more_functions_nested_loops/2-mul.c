09caab5693608e22e3185cde796740b8c70caad4
KPMuriungi <kpmuriungi@gmail.com>
2022-07-07 20:05:00 +0000

    More funtions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two parameters
+ *
+ * @a: Parameter
+ *
+ * @b: Parameter
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (0);
+}
