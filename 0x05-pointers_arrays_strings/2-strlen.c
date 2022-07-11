ef1424e5d48234e0215ef88dbe2546527cc586a8
KPMuriungi <kpmuriungi@gmail.com>
2022-07-11 18:07:58 +0000

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _strlen - finds length of a string
+ *
+ * @s: String pointer
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len
+
+	len = _strlen(*c);
+
+	return (len);
+}
