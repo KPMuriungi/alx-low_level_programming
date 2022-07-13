9dd566d34c9ffb7a9073854a57643a239c2ace0c
KPMuriungi <kpmuriungi@gmail.com>
2022-07-13 21:14:27 +0000

    arrays

---
+++ 0x06-pointers_arrays_strings/3-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: String pointer
+ *
+ * @s2: String pointer
+ *
+ * Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			return (0);
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++
+	}
+}
