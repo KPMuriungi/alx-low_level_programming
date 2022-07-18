04523c827a22760938eb90c95a8b142c2fef0ac2
KPMuriungi <kpmuriungi@gmail.com>
2022-07-18 12:29:27 -0700

    Pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills the first n bytes of the memory
+ *
+ * @s: String pointer
+ *
+ * @b: String point
+ *
+ * @n: Parameter
+ *
+ * Return: s
+ */
+
+char *memset(char *s, char *b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
