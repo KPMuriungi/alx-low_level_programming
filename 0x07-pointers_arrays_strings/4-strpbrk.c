cf0ee26d838476cd5d95f5a0aa76e9ab3057461e
KPMuriungi <kpmuriungi@gmail.com>
2022-07-18 22:03:35 +0000

    Pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - function that copies the n bytes from memory area src to dest
+ *
+ * @s: String pointer
+ *
+ * @accept: String pointer
+ *
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, n;
+
+	n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			return (NULL);
+		}
+	}
+}
