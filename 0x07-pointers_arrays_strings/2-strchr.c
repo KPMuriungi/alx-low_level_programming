535a99a3f79b896b967c79218bd68f43f78f2ff2
KPMuriungi <kpmuriungi@gmail.com>
2022-07-18 21:05:20 +0000

    Pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - function that copies the n bytes from memory area src to dest
+ *
+ * @s: String pointer
+ *
+ * @c: String pointer
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, n;
+
+	i = 0;
+	n = 0;
+
+	while (s[n] != 0)
+	{
+		n++;
+	}
+
+	while (i < n)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		else
+		{
+			return (NULL);
+		}
+		i++;
+	}
+	return (0);
+}
