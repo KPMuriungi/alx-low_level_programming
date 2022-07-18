b5abbad05c342ca83c4d5bc913488c53775882ac
KPMuriungi <kpmuriungi@gmail.com>
2022-07-18 20:41:40 +0000

    Pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,9 +16,12 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+
+	while (i < n)
 	{
-		src[i] = dest[i];
+		dest[i] = src[i];
+		i++;
 	}
 	return (dest);
 }
