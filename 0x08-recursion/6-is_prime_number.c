dad529656b33b335b767aa846ad279fddb6e8157
KPMuriungi <kpmuriungi@gmail.com>
2022-07-21 22:24:42 +0000

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * check_prime - checks for prime numbers
+ *
+ * @num: square integer
+ *
+ * @x: integer
+ *
+ * Return: 0 or 1
+ */
+
+int check_prime(int num, int x)
+{
+	if (num % x == 0 && x > 1)
+	{
+		return (0);
+	}
+	return (check_prime(num, x + 1));
+}
+
+/**
+ * is_prime_number - returns the natural square root of a number
+ *
+ * @n: integer
+ *
+ * Return: 0 or 1
+ */
+
+int is_prime_number(int n)
+{
+	int x;
+
+	x = 1;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (check_prime(n, x + 1));
+}
