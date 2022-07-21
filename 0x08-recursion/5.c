9c6a385527ffea7d72682cd4e5f1614a5e8d7ce9
KPMuriungi <kpmuriungi@gmail.com>
2022-07-21 21:41:39 +0000

    Recursion

--- 0x08-recursion/5.c
+++ 0x08-recursion/5.c
@@ -7,22 +7,18 @@
  *
  * @x: integer
  *
- * Return: 
+ * Return: square
  */
 
-int check_int(int square, int x)
+int check_square(int square, int x)
 {
 	if (x * x == square)
 	{
 		return (x);
 	}
-	else if (x > square / x)
-	{
-		return (check_int(square, x - 1));
-	}
 	else if (x < square / x)
 	{
-		return (check_int(square, x + 1));
+		return (check_square(square, x + 1));
 	}
 	else
 	{
@@ -31,21 +27,29 @@ int check_int(int square, int x)
 }
 
 /**
- *  * _sqrt_recursion - returns the natural square root of a number
- *   * @n: integer
- *    * Return: integer - square root, if none returns -1
- *     */
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: integer
+ *
+ * Return: integer - square root, if none returns -1
+ */
 
 int _sqrt_recursion(int n)
 {
-		int start;
+	int start;
 
-			start = 1;
+	start = 1;
 
-				if (n < 0)
-							return (-1);
-					else if (n == 1)
-								return (1);
-						else
-									return (check_int(n, start));
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (check_square(n, start));
+	}
 }
