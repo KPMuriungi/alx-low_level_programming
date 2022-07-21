1a579c2ebbeba871a444d95efd4610ead3b63697
KPMuriungi <kpmuriungi@gmail.com>
2022-07-21 10:30:04 +0000

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,70 @@
+#include "main.h"
 
+/**
+ * strlength - Prints the length of a string
+ *
+ * @s: String pointer
+ *
+ * Return: Length of the string
+ */
 
+int strlength(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (strlength(s + 1) + 1);
+}
+
+/**
+ * check_palindrome - Checks for a palindrome
+ *
+ * @s: String pointer
+ *
+ * @a: Parameter
+ *
+ * @z: Parameter
+ *
+ * Return: 0
+ */
+
+int check_palindrome(char *s, int a, int z)
+{
+	if (s[a] != s[z])
+	{
+		return (0);
+	}
+	if (a >= z)
+	{
+		return (1);
+	}
+	if (s[a] == s[z])
+	{
+		return (check_palindrome(s, a + 1, z - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - Recursive function
+ *
+ * @s: String pointer
+ *
+ * Return: 0
+ */
 
 int is_palindrome(char *s)
-	int x, y;
-	int x =0;
+{
+	int len, a, z;
 
+	len = strlength(s);
+	a = 0;
+	z = len - 1;
+
+	if (len == 0 || len == 1)
+	{
+		return (1);
+	}
+	return (check_palindrome(s, a, z));
+}
