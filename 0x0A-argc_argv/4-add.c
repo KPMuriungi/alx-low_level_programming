ae16961850bd125f404b53e0bcbee085a9593d36
KPMuriungi <kpmuriungi@gmail.com>
2022-07-22 22:51:07 +0000

    argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - function
+ *
+ * @argc: Argument count
+ *
+ * @argv: Arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{	
+					printf("Error\n");
+					return (1);
+				
+				}
+			}
+			
+			sum += atoi(argv[i]);
+		}
+		
+		printf("%d\n", sum);
+	}	
+
+	return (0);
+}
