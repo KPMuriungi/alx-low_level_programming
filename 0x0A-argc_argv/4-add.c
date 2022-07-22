158c9ca35daeb737bf678c2d76b1d2f2b1318b9b
KPMuriungi <kpmuriungi@gmail.com>
2022-07-22 22:51:54 +0000

    argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,18 +27,18 @@ int main(int argc, char *argv[])
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
 				if (!isdigit(argv[i][j]))
-				{	
+				{
 					printf("Error\n");
 					return (1);
-				
+
 				}
 			}
-			
+
 			sum += atoi(argv[i]);
 		}
-		
+
 		printf("%d\n", sum);
-	}	
+	}
 
 	return (0);
 }
