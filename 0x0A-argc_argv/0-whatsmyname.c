81ac6b6204ea260191125157435de8f3b834c56f
KPMuriungi <kpmuriungi@gmail.com>
2022-07-22 21:01:13 +0000

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc: Argument count
+ *
+ * @argv: Arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
