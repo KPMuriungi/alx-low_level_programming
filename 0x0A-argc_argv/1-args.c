439adcd2af661961b04dc145bd60e36649f504ba
KPMuriungi <kpmuriungi@gmail.com>
2022-07-22 21:10:52 +0000

    argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc: Argument count
+ *
+ * @argv: Arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
