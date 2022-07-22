33222a3291d71f4e44bbeff45d24e069a0a94381
KPMuriungi <kpmuriungi@gmail.com>
2022-07-22 22:07:39 +0000

    argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ *
+ * @argc: Argument count
+ *
+ * @argv: Arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	prod = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", prod);
+
+	return (0);
+}
