4cff75b58f74d6830e34e1f5685336feca23253a
KPMuriungi <kpmuriungi@gmail.com>
2022-07-22 23:10:31 +0000

    argc_argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - function
+ *
+ * @argc: Argument count
+ *
+ * @argv: Arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) < 0)
+		{
+			printf("0\n");
+		}
+	}
+	return (0);
+}
