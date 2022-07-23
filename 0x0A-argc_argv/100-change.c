ed4c0326d38c009d9f761d8a46c82637e62310b1
KPMuriungi <kpmuriungi@gmail.com>
2022-07-23 00:42:56 +0000

    argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,33 +16,40 @@ int main(int argc, char *argv[])
 	int x, cents, coins;
 
 	if (argc != 2)
+	{
 		printf("Error\n");
-		return (1);
+		return (1);}
 	if (atoi(argv[1]) < 0)
-		printf("0\n");
+	{
+		printf("0\n");}
 
 	cents = atoi(argv[1]);
 	coins = 0;
 
 	if (cents >= 25)
+	{
 		x = cents / 25;
 		cents = cents % 25;
-		coins += x;
+		coins += x;}
 	if (cents >= 10 && cents < 25)
+	{
 		x = cents / 10;
 		cents = cents % 10;
-		coins += x;
+		coins += x;}
 	if (cents >= 5 && cents < 10)
+	{
 		x = cents / 5;
 		cents = cents % 5;
-		coins += x;
+		coins += x;}
 	if (cents >= 2 && cents < 5)
+	{
 		x = cents / 2;
 		cents = cents % 2;
-		coins += x;
+		coins += x;}
 	if (cents == 1)
+	{
 		x = cents;
-		coins += x;
+		coins += x;}
 
 	printf("%d\n", coins);
 
