d0dab5fd8762a80b5a0ff9b295b1651d8b1c1ce4
KPMuriungi <kpmuriungi@gmail.com>
2022-07-26 21:39:43 +0000

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,16 +14,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, j, k;
+	int i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	i = j = 0;
 	while (s1[i] != '\0')
@@ -42,13 +38,18 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	for (k = 0; k < i; i++)
+
+	i = j = 0;
+	while (s1[i] != '\0')
 	{
-		str[k] = s1[k];
+		str[i] = s1[i];
 	}
-	for (k = 0; k < j; k++)
+	while (s2[j] != '\0')
 	{
-		str[k + i] = s2[k];
+		str[i] = s2[j];
+		i++, j++;
 	}
+	str[i] = '\0';
+
 	return (str);
 }
