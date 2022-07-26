607833bfbca3167e7fdd0ee68451230454801327
KPMuriungi <kpmuriungi@gmail.com>
2022-07-26 21:32:09 +0000

    Malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,19 +14,22 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, j;
+	int i = 0;
+	int j = 0;
+	int k;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
 	while (s1[i] != '\0')
 	{
 		i++;
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	while (s2[j] != '\0')
 	{
