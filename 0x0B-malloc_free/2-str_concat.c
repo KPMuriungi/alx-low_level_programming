0fa86e5546bc8305527a763982546f9b6296095d
KPMuriungi <kpmuriungi@gmail.com>
2022-07-26 21:51:12 +0000

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,43 +13,37 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *str;
-	int i, j;
+	int length1, length2, length3, i;
+	char *arr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	i = j = 0;
-	while (s1[i] != '\0')
+	length1 = 0;
+	while (*(s1 + length1) != '\0')
 	{
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		j++;
+		length1++;
 	}
 
-	str = malloc(sizeof(char) * (i + j + 1));
-
-	if (str == NULL)
+	length2 = 0;
+	while (*(s2 + length2) != '\0')
 	{
+		length2++;
+	}
+
+	length3 = length1 + length2;
+
+	arr = malloc((sizeof(char) * length3) + 1);
+
+	if (arr == NULL)
 		return (NULL);
-	}
 
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		str[i] = s2[j];
-		i++, j++;
-	}
-	str[i] = '\0';
+	for (i = 0; i < length1; i++)
+		arr[i] = s1[i];
+	for (i = 0; i < length2; i++)
+		arr[i + length1] = s2[i];
 
-	return (str);
+	return (arr);
 }
---
+++ 0x0B-malloc_free/2.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function
+ *
+ * @s1: Parameter
+ *
+ * @s2: Parameter
+ *
+ * Return: 0
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	str = malloc(sizeof(char) * (i + j + 1));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++, j++;
+	}
+	str[i] = '\0';
+
+	return (str);
+}
